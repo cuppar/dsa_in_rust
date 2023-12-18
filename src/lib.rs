c1bab3b4a81f26521a6661b3116a9f4def06075e
cuppar <cuppar@163.com>
2023-12-18 17:20:37 +0800

    link deque

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod link_queue;
 pub mod array_queue;
 pub mod array_stack;
 pub mod link_stack;
+pub mod link_deque;
--- src/link_deque.rs
+++ src/link_deque.rs
@@ -1,15 +1,14 @@
 use std::{
-    cell::{Ref, RefCell},
+    cell::{RefCell, RefMut},
     fmt::Debug,
     rc::Rc,
     vec,
 };
 
 #[derive(Debug)]
-pub struct LinkQueue<T: Debug> {
+pub struct LinkDeque<T: Debug> {
     front: Option<Link<T>>,
     rear: Option<Link<T>>,
-    size: usize,
 }
 
 type Link<T> = Rc<RefCell<Node<T>>>;
@@ -17,68 +16,104 @@ type Link<T> = Rc<RefCell<Node<T>>>;
 #[derive(Debug)]
 struct Node<T: Debug> {
     val: T,
+    prev: Option<Link<T>>,
     next: Option<Link<T>>,
 }
 
 impl<T: Debug> Node<T> {
     fn new(val: T) -> Link<T> {
-        Rc::new(RefCell::new(Node { val, next: None }))
+        Rc::new(RefCell::new(Node {
+            val,
+            prev: None,
+            next: None,
+        }))
     }
 }
 
-impl<T: Clone + Debug> LinkQueue<T> {
+impl<T: Clone + Debug> LinkDeque<T> {
     pub fn new() -> Self {
-        LinkQueue {
+        LinkDeque {
             front: None,
             rear: None,
-            size: 0,
         }
     }
 
-    pub fn push(&mut self, val: T) {
+    pub fn push_front(&mut self, val: T) {
+        let new_front = Node::new(val);
+        if let Some(old_front) = self.front.take() {
+            old_front.borrow_mut().prev = Some(Rc::clone(&new_front));
+            new_front.borrow_mut().next = Some(old_front);
+        } else {
+            self.rear = Some(Rc::clone(&new_front));
+        }
+        self.front = Some(new_front);
+    }
+
+    pub fn push_rear(&mut self, val: T) {
         let new_rear = Node::new(val);
         if let Some(old_rear) = self.rear.take() {
-            old_rear.borrow_mut().next = Some(new_rear.clone());
-            self.rear = Some(new_rear);
+            old_rear.borrow_mut().next = Some(Rc::clone(&new_rear));
+            new_rear.borrow_mut().prev = Some(old_rear);
         } else {
-            self.front = Some(new_rear.clone());
-            self.rear = Some(new_rear);
+            self.front = Some(Rc::clone(&new_rear));
         }
-        self.size += 1;
+        self.rear = Some(new_rear);
     }
 
-    pub fn pop(&mut self) -> Option<T> {
+    pub fn pop_front(&mut self) -> Option<T> {
         self.front.take().map(|old_front| {
             match old_front.borrow_mut().next.take() {
                 Some(new_front) => {
+                    new_front.borrow_mut().prev.take();
                     self.front = Some(new_front);
                 }
                 None => {
-                    self.rear = None;
+                    self.rear.take();
                 }
             }
-            self.size -= 1;
             Rc::try_unwrap(old_front)
                 .ok()
                 .unwrap()
                 .into_inner()
                 .val
-                .clone()
+                .clone() // workaround about debug with Drop impl for Node<T>
         })
     }
 
-    pub fn peek(&self) -> Option<Ref<'_, T>> {
+    pub fn pop_rear(&mut self) -> Option<T> {
+        self.rear.take().map(|old_rear| {
+            match old_rear.borrow_mut().prev.take() {
+                Some(new_rear) => {
+                    new_rear.borrow_mut().next.take();
+                    self.rear = Some(new_rear);
+                }
+                None => {
+                    self.front.take();
+                }
+            }
+            Rc::try_unwrap(old_rear)
+                .ok()
+                .unwrap()
+                .into_inner()
+                .val
+                .clone() // workaround about debug with Drop impl for Node<T>
+        })
+    }
+
+    pub fn peek_front(&self) -> Option<RefMut<'_, T>> {
         self.front
             .as_ref()
-            .map(|node| Ref::map(node.borrow(), |node| &node.val))
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.val))
+    }
+
+    pub fn peek_rear(&self) -> Option<RefMut<'_, T>> {
+        self.rear
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.val))
     }
 
     pub fn is_empty(&self) -> bool {
-        self.size == 0
-    }
-
-    pub fn size(&self) -> usize {
-        self.size
+        self.front.is_none() && self.rear.is_none()
     }
 
     pub fn to_vec(&self) -> Vec<T> {
@@ -103,19 +138,18 @@ impl<T: Debug> Drop for Node<T> {
     }
 }
 
-impl<T: Debug> Drop for LinkQueue<T> {
+impl<T: Debug> Drop for LinkDeque<T> {
     fn drop(&mut self) {
         let mut cur_node = self.front.take();
+
         while let Some(node) = cur_node {
-            if let Ok(node) = Rc::try_unwrap(node) {
-                cur_node = node.borrow_mut().next.take();
-                // node go out of scope
+            if let Some(next_node) = node.borrow_mut().next.take() {
+                next_node.borrow_mut().prev.take();
+                cur_node = Some(next_node);
             } else {
-                // cur point to last node which `rear` point to, too.
-                // last node will drop when `rear` go out of scope.
                 break;
-                // node go out of scope
             }
+            // node go out of scope
         }
     }
 }
@@ -125,27 +159,48 @@ mod tests {
     use super::*;
 
     #[test]
-    fn push_pop() {
-        let mut queue = LinkQueue::new();
-        queue.push(1);
-        queue.push(2);
-        queue.push(3);
+    fn front() {
+        let mut deque = LinkDeque::new();
+        deque.push_front(1);
+        deque.push_front(2);
+        deque.push_front(3);
 
-        assert_eq!(queue.size(), 3);
-        assert!(!queue.is_empty());
+        assert!(!deque.is_empty());
 
-        assert_eq!(queue.to_vec(), [1, 2, 3]);
+        assert_eq!(deque.to_vec(), [3, 2, 1]);
 
-        assert_eq!(*queue.peek().unwrap(), 1);
-        assert_eq!(queue.pop(), Some(1));
-        assert_eq!(*queue.peek().unwrap(), 2);
-        assert_eq!(queue.pop(), Some(2));
-        assert_eq!(*queue.peek().unwrap(), 3);
-        assert_eq!(queue.pop(), Some(3));
-        assert_eq!(queue.pop(), None);
-        assert_eq!(queue.pop(), None);
+        assert_eq!(*deque.peek_front().unwrap(), 3);
+        assert_eq!(deque.pop_front(), Some(3));
+        assert_eq!(*deque.peek_front().unwrap(), 2);
+        assert_eq!(deque.pop_front(), Some(2));
+        assert_eq!(*deque.peek_front().unwrap(), 1);
+        assert_eq!(deque.pop_front(), Some(1));
+        assert_eq!(deque.pop_front(), None);
+        assert_eq!(deque.pop_front(), None);
 
-        assert_eq!(queue.size(), 0);
-        assert!(queue.is_empty());
+        assert!(deque.is_empty());
+    }
+
+    #[test]
+    fn rear() {
+        let mut deque = LinkDeque::new();
+        deque.push_rear(1);
+        deque.push_rear(2);
+        deque.push_rear(3);
+
+        assert!(!deque.is_empty());
+
+        assert_eq!(deque.to_vec(), [1, 2, 3]);
+
+        assert_eq!(*deque.peek_rear().unwrap(), 3);
+        assert_eq!(deque.pop_rear(), Some(3));
+        assert_eq!(*deque.peek_rear().unwrap(), 2);
+        assert_eq!(deque.pop_rear(), Some(2));
+        assert_eq!(*deque.peek_rear().unwrap(), 1);
+        assert_eq!(deque.pop_rear(), Some(1));
+        assert_eq!(deque.pop_rear(), None);
+        assert_eq!(deque.pop_rear(), None);
+
+        assert!(deque.is_empty());
     }
 }
