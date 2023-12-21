c1b1e72464b4422e8b67a6320ea172d5da8b1545
cuppar <cuppar@163.com>
2023-12-21 19:51:45 +0800

    binary search tree, insert

---
+++ src/tree/binary_search_tree.rs
@@ -0,0 +1,216 @@
+use std::{
+    cell::{RefCell, RefMut},
+    collections::VecDeque,
+    fmt::Debug,
+    rc::Rc,
+};
+
+pub struct BinarySearchTree<T: Clone + Debug + Ord> {
+    root: Option<Edge<T>>,
+}
+pub struct BinarySearchTreeNode<T: Clone + Debug + Ord> {
+    val: T,
+    left: Option<Edge<T>>,
+    right: Option<Edge<T>>,
+}
+
+type Edge<T> = Rc<RefCell<BinarySearchTreeNode<T>>>;
+
+impl<T: Clone + Debug + Ord> BinarySearchTreeNode<T> {
+    fn new(val: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            val,
+            left: None,
+            right: None,
+        }))
+    }
+}
+
+impl<T: Clone + Debug + Ord> BinarySearchTree<T> {
+    pub fn new() -> Self {
+        Self { root: None }
+    }
+
+    pub fn insert_iterate(&mut self, val: T) {
+        if self.root.is_none() {
+            self.root = Some(BinarySearchTreeNode::new(val));
+            return;
+        }
+
+        let mut cur = self.root.clone();
+        let mut pre = None;
+
+        while let Some(node) = cur.clone() {
+            use std::cmp::Ordering::*;
+            pre = cur;
+            match node.borrow().val.cmp(&val) {
+                Less => {
+                    cur = node.borrow().right.clone();
+                }
+                Equal => return,
+                Greater => {
+                    cur = node.borrow().left.clone();
+                }
+            }
+        }
+
+        let pre = pre.unwrap();
+        let new_node = BinarySearchTreeNode::new(val.clone());
+        if pre.borrow().val > val {
+            pre.borrow_mut().left = Some(new_node);
+        } else {
+            pre.borrow_mut().right = Some(new_node);
+        }
+    }
+    pub fn insert_recursion(&mut self, val: T) {
+        Self::_insert_recursion(&mut self.root, val);
+    }
+
+    pub fn _insert_recursion(root: &mut Option<Edge<T>>, val: T) {
+        if let Some(root) = root {
+            use std::cmp::Ordering::*;
+            let val_in_node = root.borrow().val.clone();
+
+            let (mut left, mut right) =
+                RefMut::map_split(root.borrow_mut(), |root| (&mut root.left, &mut root.right));
+            match val_in_node.cmp(&val) {
+                Less => {
+                    Self::_insert_recursion(&mut right, val);
+                }
+                Equal => return,
+                Greater => {
+                    Self::_insert_recursion(&mut left, val);
+                }
+            }
+        } else {
+            *root = Some(BinarySearchTreeNode::new(val))
+        }
+    }
+
+    pub fn print(&self) {
+        Self::_print(&self.root, 0, "Root: ");
+    }
+
+    pub fn _print(node: &Option<Edge<T>>, level: usize, prefix: &str) {
+        for _ in 0..level {
+            print!("  ");
+        }
+        if let Some(node) = node {
+            println!("{}{:?}", prefix, node.borrow().val);
+            let new_level = level + 1;
+            Self::_print(
+                &node.borrow().left,
+                new_level,
+                format!("L{}: ", new_level).as_str(),
+            );
+            Self::_print(
+                &node.borrow().right,
+                new_level,
+                format!("R{}: ", new_level).as_str(),
+            );
+        } else {
+            println!("{}<None>", prefix);
+        }
+    }
+
+    pub fn level_order(&self) -> Vec<T> {
+        let mut result = vec![];
+        if let Some(root) = &self.root {
+            let mut que = VecDeque::new();
+            que.push_back(Rc::clone(root));
+
+            while let Some(node) = que.pop_front() {
+                result.push(node.borrow().val.clone());
+                if let Some(left) = node.borrow().left.as_ref() {
+                    que.push_back(Rc::clone(left));
+                }
+                if let Some(right) = node.borrow().right.as_ref() {
+                    que.push_back(Rc::clone(right));
+                }
+            }
+        }
+        result
+    }
+
+    pub fn pre_order(&self) -> Vec<T> {
+        let mut result = vec![];
+        Self::_pre_order(&self.root, &mut result);
+        result
+    }
+    fn _pre_order(node: &Option<Edge<T>>, result: &mut Vec<T>) {
+        if let Some(node) = node {
+            result.push(node.borrow().val.clone());
+            Self::_pre_order(&node.borrow().left, result);
+            Self::_pre_order(&node.borrow().right, result);
+        }
+    }
+
+    pub fn in_order(&self) -> Vec<T> {
+        let mut result = vec![];
+        Self::_in_order(&self.root, &mut result);
+        result
+    }
+    fn _in_order(node: &Option<Edge<T>>, result: &mut Vec<T>) {
+        if let Some(node) = node {
+            Self::_in_order(&node.borrow().left, result);
+            result.push(node.borrow().val.clone());
+            Self::_in_order(&node.borrow().right, result);
+        }
+    }
+
+    pub fn post_order(&self) -> Vec<T> {
+        let mut result = vec![];
+        Self::_post_order(&self.root, &mut result);
+        result
+    }
+    fn _post_order(node: &Option<Edge<T>>, result: &mut Vec<T>) {
+        if let Some(node) = node {
+            Self::_post_order(&node.borrow().left, result);
+            Self::_post_order(&node.borrow().right, result);
+            result.push(node.borrow().val.clone());
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn iterate() {
+        let mut tree = BinarySearchTree::new();
+        tree.print();
+
+        tree.insert_iterate(3);
+        tree.insert_iterate(5);
+        tree.insert_iterate(1);
+        tree.insert_iterate(2);
+        tree.insert_iterate(4);
+
+        tree.print();
+
+        println!("Level order: {:?}", tree.level_order());
+        println!("Pre order: {:?}", tree.pre_order());
+        println!("In order: {:?}", tree.in_order());
+        println!("Post order: {:?}", tree.post_order());
+    }
+
+    #[test]
+    fn recursion() {
+        let mut tree = BinarySearchTree::new();
+        tree.print();
+
+        tree.insert_recursion(3);
+        tree.insert_recursion(5);
+        tree.insert_recursion(1);
+        tree.insert_recursion(2);
+        tree.insert_recursion(4);
+
+        tree.print();
+
+        println!("Level order: {:?}", tree.level_order());
+        println!("Pre order: {:?}", tree.pre_order());
+        println!("In order: {:?}", tree.in_order());
+        println!("Post order: {:?}", tree.post_order());
+    }
+}
